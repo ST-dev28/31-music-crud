724a3559b76b7980cd0f50a1b7096827e1893e30
SandraT <sndrbltr@gmail.com>
2021-07-16 16:35:35 +0000

    Playlist done

--- Artist.js
+++ Artist.js
@@ -4,61 +4,50 @@ class Artist {
         this.list = [];
         this.name = name;
         this.curr = curr;
+        this.profit = 0;
+        this.songs = [];
+        this.pricePerSong = 0;
     }
 
     intro() {
         console.log(`Hi, my name is ${this.name} and I am a musician!`);
     }
+
     songPrice(price) {
-        this.list.price = price / 100;
-        console.log(`${this.name}'s price per song is ${(this.list.price).toFixed(2)} ${this.curr}`);
-        return price;
+        this.pricePerSong = price;
+        console.log(`${this.name}'s price per song is ${(this.pricePerSong / 100).toFixed(2)} ${this.curr};`);
+        console.log('---------------------');
     }
+
     addSong(song) {
-        this.list.push({
-            song: song,
-            repeat: 0,
-            price: 0,
-            profit: 0,
-        });
-        //console.log(song);       //this one
-        console.log(this.list);
+        this.list.push({ song, count: 0 });
+        console.log(song);
+        //console.log(this.list);
     }
+
     playlist() {
+        console.log('---------------------');
         console.log(`${this.name}'s playlist:`);
         console.log('========================');
         for (let i = 0; i < this.list.length; i++) {
             const song = this.list[i];
-            console.log(`${i + 1}. ${song.song} (${song.repeat}) `);
+            console.log(`${i + 1}. ${song.song} (${song.count}) `);
         }
         console.log('---------------------');
     }
+
     playSong(index) {
         //console.log(index);
-        let song = this.list[index].song;
-        for (let i = 0; i < this.list.length; i++) {
-            const song = this.list[i];
-            if (i === index) {
-                song.repeat++;
-                this.list[index].profit += this.list.price;
-            }
-            /*if (this.list[i].index !== index) {
-
-                console.log(`Playing song: ${this.list[index].song} (${index})`);
-                break;                  // radau ir baigiu
-            }*/
-            console.log(this.list[index].profit);
+        if (this.list[index]) {
+            this.list[index].count += 1;
+            this.profit += this.pricePerSong;
+            console.log(`Playing song: ${this.list[index].song}.`);
         }
-
-        console.log(`Playing song: ${song}.`);
     }
+
     fortune() {
-        let profit = 0;
-        for (let i = 0; i < this.list.length; i++) {
-            // const this.list = this.list[i];
-            profit += this.list[i].profit;
-        }
-        console.log(`Total lifetime wealth of ${this.name} is ${profit} ${this.curr} right now!`);
+        console.log('--------------------');
+        console.log(`Total lifetime wealth of ${this.name} is ${this.profit / 100} ${this.curr} right now!`);
     }
 }
 
